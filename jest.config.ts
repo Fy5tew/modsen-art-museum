4cc7649f3f417ea23719eca8bf8b88f616eb87b8
Fy5tew <nikturchinovich@mail.ru>
2024-08-05 15:53:54 +0300

    feat: initial project setup

---
+++ jest.config.ts
@@ -0,0 +1,27 @@
+export default {
+    preset: 'ts-jest',
+    testEnvironment: 'jsdom',
+
+    transform: {
+        '^.+\\.tsx?$': [
+            'ts-jest',
+            {
+                'tsconfig': 'tsconfig.app.json',
+            },
+        ],
+    },
+
+    moduleNameMapper: {
+        '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+        '^.+\\.svg$': 'jest-transformer-svg',
+        '^#/(.*)$': '<rootDir>/src/$1',
+        '^#components/(.*)$': '<rootDir>/src/components/$1',
+        '^#pages/(.*)$': '<rootDir>/src/pages/$1',
+        '^#hooks/(.*)$': '<rootDir>/src/hooks/$1',
+        '^#utils/(.*)$': '<rootDir>/src/utils/$1',
+    },
+
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+};
---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import path from 'path';
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react(), tsconfigPaths()],
+    resolve: {
+        alias: {
+            '#': path.resolve(__dirname, './src/'),
+            '#components': path.resolve(__dirname, './src/components/'),
+            '#pages': path.resolve(__dirname, './src/pages/'),
+            '#hooks': path.resolve(__dirname, './src/hooks/'),
+            '#utils': path.resolve(__dirname, './src/utils/'),
+        },
+    },
+});

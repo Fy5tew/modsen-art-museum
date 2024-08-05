572e63c7b9421afb8bd29cb0538286b35d86b6b7
Fy5tew <nikturchinovich@mail.ru>
2024-08-05 17:40:52 +0300

    feat: added NotFoundPage component

---
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,11 @@
+import { PageLayout } from '#/components/PageLayout';
+
+function NotFoundPage() {
+    return (
+        <PageLayout>
+            <h1>Page Not Found</h1>
+        </PageLayout>
+    );
+}
+
+export { NotFoundPage };

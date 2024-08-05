68b57a774659167fb65a8348d843786a5634507f
Fy5tew <nikturchinovich@mail.ru>
2024-08-05 17:40:25 +0300

    feat: added FavoritesPage component

---
+++ src/pages/FavoritesPage/FavoritesPage.tsx
@@ -0,0 +1,11 @@
+import { PageLayout } from '#/components/PageLayout';
+
+function FavoritesPage() {
+    return (
+        <PageLayout>
+            <h1>Favorites Page</h1>
+        </PageLayout>
+    );
+}
+
+export { FavoritesPage };

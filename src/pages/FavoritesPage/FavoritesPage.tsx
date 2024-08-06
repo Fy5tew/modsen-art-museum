bddd8b33692b67e00a1f3c017f83df6ebbfbb198
Fy5tew <nikturchinovich@mail.ru>
2024-08-07 01:54:44 +0300

    feat: added content for favorites page

--- src/pages/FavoritesPage/FavoritesPage.tsx
+++ src/pages/FavoritesPage/FavoritesPage.tsx
@@ -1,9 +1,53 @@
 import { PageLayout } from '#/components/PageLayout';
+import { PageHeader } from '#/components/PageHeader';
+import { PageDoubleHeader } from '#/components/PageDoubleHeader';
+import { PaintingList } from '#/components/PaintingList';
+import { useFavorites } from '#/hooks/useFavorites';
+import bookmarkSecondaryIconSrc from '/icon/bookmark-secondary.svg';
+import styles from './FavoritesPage.module.scss';
 
 function FavoritesPage() {
+    const favorites = useFavorites();
+
     return (
         <PageLayout>
-            <h1>Favorites Page</h1>
+            <div className={styles.pageHeaderWrapper}>
+                <PageHeader>Here are your</PageHeader>
+                <PageHeader>
+                    <span className={styles.highlighted}>
+                        <img
+                            className={styles.icon}
+                            src={bookmarkSecondaryIconSrc}
+                            alt=""
+                        />
+                        <mark>Favorites</mark>
+                    </span>
+                </PageHeader>
+            </div>
+            <div className={styles.favoritesHeaderWrapper}>
+                <PageDoubleHeader
+                    subHeader="Saved by you"
+                    mainHeader="Your favorites list"
+                />
+            </div>
+            <div className={styles.favoritesListWrapper}>
+                {favorites.favorites?.length ? (
+                    <PaintingList
+                        paintings={favorites.favorites}
+                        onClick={(id) => console.log(id)}
+                        isFavorite={(id) =>
+                            favorites.favorites?.some((p) => p.id === id) ??
+                            false
+                        }
+                        onFavoriteAdd={favorites.add}
+                        onFavoriteRemove={favorites.remove}
+                    />
+                ) : (
+                    <h3 className={styles.emptyList}>
+                        Your favorites list is empty
+                    </h3>
+                )}
+            </div>
         </PageLayout>
     );
 }

fe0b4644f0ee335bb5e865924c3fcd3042dcb56a
Fy5tew <nikturchinovich@mail.ru>
2024-08-06 16:46:38 +0300

    feat: added header and footer

--- src/components/PageLayout/PageLayout.tsx
+++ src/components/PageLayout/PageLayout.tsx
@@ -1,11 +1,32 @@
 import { ReactNode } from 'react';
+import { ContentContainer } from '#/components/ContentContainer';
+import { Header } from '#components/Header';
+import { Footer } from '#components/Footer';
+
+import styles from './PageLayout.module.scss';
 
 type PageLayoutProps = {
     children: ReactNode;
 };
 
 function PageLayout({ children }: PageLayoutProps) {
-    return <div>{children}</div>;
+    return (
+        <div className={styles.pageWrapper}>
+            <div className={styles.headerWrapper}>
+                <ContentContainer>
+                    <Header />
+                </ContentContainer>
+            </div>
+            <div className={styles.contentWrapper}>
+                <ContentContainer className={styles.content}>
+                    {children}
+                </ContentContainer>
+            </div>
+            <div className={styles.footerWraper}>
+                <Footer />
+            </div>
+        </div>
+    );
 }
 
 export type { PageLayoutProps };

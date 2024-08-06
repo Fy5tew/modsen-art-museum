a97c807aa1f1d6a6b25e41075b2ba6d78d1c79a3
Fy5tew <nikturchinovich@mail.ru>
2024-08-07 01:58:09 +0300

    feat: updated not found page content

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,9 +1,13 @@
 import { PageLayout } from '#/components/PageLayout';
+import { PageHeader } from '#/components/PageHeader';
 
 function NotFoundPage() {
     return (
         <PageLayout>
-            <h1>Page Not Found</h1>
+            <PageHeader>
+                <mark>404</mark>
+            </PageHeader>
+            <PageHeader>This page is not exists</PageHeader>
         </PageLayout>
     );
 }

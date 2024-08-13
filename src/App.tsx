e7c728a9368cb4ef4362e0e55ce9e68de4cc0d6f
Fy5tew <nikturchinovich@mail.ru>
2024-08-13 23:49:03 +0300

    feat: added MenuContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+import { MenuContextProvider } from './contexts/MenuContext';
 import { ROUTES } from './routes';
 
 export function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                {Object.values(ROUTES).map((r) => (
-                    <Route
-                        key={r.basePath}
-                        path={r.getRoutePath()}
-                        Component={r.component}
-                    />
-                ))}
-            </Routes>
-        </BrowserRouter>
+        <MenuContextProvider>
+            <BrowserRouter>
+                <Routes>
+                    {Object.values(ROUTES).map((r) => (
+                        <Route
+                            key={r.basePath}
+                            path={r.getRoutePath()}
+                            Component={r.component}
+                        />
+                    ))}
+                </Routes>
+            </BrowserRouter>
+        </MenuContextProvider>
     );
 }
